3c4d0d6eebb1c007b32c5c59d780962e897f1403
gram <git@orsinium.dev>
2024-03-29 17:43:47 +0100

    fix Go SDK import name

--- firefly/graphics_bindings.go
+++ firefly/graphics_bindings.go
@@ -5,7 +5,7 @@ func drawPoint(
 	x, y, c int32,
 )
 
-//go:wasmimport graphics draw_triange
+//go:wasmimport graphics draw_triangle
 func drawTriangle(
 	p1_x, p1_y, p2_x, p2_y, p3_x, p3_y, fill_color, stroke_color, stroke_width int32,
 )
