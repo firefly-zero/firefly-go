3e3af48030fb5b77983c13b7529525b4b2cf6423
gram <git@orsinium.dev>
2024-04-08 14:45:25 +0200

    draft image demo

---
+++ _examples/image/image.go
@@ -0,0 +1,19 @@
+// Image source: https://opengameart.org/content/zelda-like-tilesets-and-sprites
+package main
+
+import "github.com/life4/firefly-go/firefly"
+
+func init() {
+	firefly.Boot = boot
+	firefly.Render = render
+}
+
+var font firefly.Font
+
+func boot() {
+	panic("")
+}
+
+func render() {
+	panic("")
+}
