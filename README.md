3b9278413b462608307505cfa409bbaabb477850
gram <git@orsinium.dev>
2024-05-06 14:49:39 +0200

    readme

---
+++ README.md
@@ -0,0 +1,17 @@
+# firefly-go
+
+The Go SDK for making [Firefly Zero](https://fireflyzero.com/) games.
+
+* [▶️ getting started](https://docs.fireflyzero.com/dev/getting-started/)
+* [📄 api docs](https://pkg.go.dev/github.com/firefly-zero/firefly-go)
+* [🐙 github](https://github.com/firefly-zero/firefly-go)
+
+## Installation
+
+```bash
+go get github.com/firefly-zero/firefly-go
+```
+
+## License
+
+MIT License. You can do whatever you want with the SDK, modify it, embed into any apps and games. Have fun!
--- firefly/callbacks.go
+++ firefly/callbacks.go
@@ -1,9 +1,27 @@
 package firefly
 
 var (
-	Boot       func()
-	Update     func()
-	Render     func()
+	// Callback to be called once when the app starts.
+	//
+	// Called after memory is initialized and host functions are registered
+	// but before any other callback.
+	Boot func()
+
+	// Callback to be called on every update.
+	//
+	// Don't use it to draw on the screen, use [Render] instead.
+	Update func()
+
+	// Callback to be called before rendering the frame.
+	//
+	// Don't use it to update the state, use [Update] instead.
+	Render func()
+
+	// Callback to be called before rendering a horizontal line on the screen.
+	//
+	// Accepts the index of the line about to be rendered
+	// and returns the index of the line for which it should be called next time.
+	// Use it to update color palette to support more than 4 colors per frame.
 	RenderLine func(int) int
 )
 
