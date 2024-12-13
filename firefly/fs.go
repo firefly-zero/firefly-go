6e4afd5acd01339fd8c3be965fdb3ce981d3231e
gram <git@orsinium.dev>
2024-12-13 20:02:02 +0100

    make LoadFile binary smaller

--- firefly/fs.go
+++ firefly/fs.go
@@ -17,21 +17,50 @@ func (f File) Image() Image {
 	return Image{f.Raw}
 }
 
+// Ensure that the loaded file exists.
+func (f File) Must() File {
+	if f.Raw == nil {
+		panic("file not found")
+	}
+	return f
+}
+
 // Read a file.
 //
 // It will first lookup file in the app's ROM directory and then check
 // the app writable data directory.
 //
 // If the file does not exist, the Raw value of the returned File will be nil.
+//
+// The second argument is the buffer in which the file should be loaded.
+// If the buffer is smaller than the file content, it gets cut.
+// If the buffer is nil, a new buffer of sufficient size will be allocated.
 func LoadFile(path string, buf []byte) File {
-	pathPtr := unsafe.Pointer(unsafe.StringData(path))
 	if buf == nil {
-		fileSize := getFileSize(pathPtr, uint32(len(path)))
-		if fileSize == 0 {
-			return File{nil}
-		}
-		buf = make([]byte, fileSize)
+		return loadAllocFile(path)
 	}
+	return loadFileInto(path, buf)
+}
+
+// Allocate a new buffer and load the file into it.
+func loadAllocFile(path string) File {
+	pathPtr := unsafe.Pointer(unsafe.StringData(path))
+	fileSize := getFileSize(pathPtr, uint32(len(path)))
+	if fileSize == 0 {
+		return File{nil}
+	}
+	buf := make([]byte, fileSize)
+	bufPtr := unsafe.Pointer(unsafe.SliceData(buf))
+	loadFile(
+		pathPtr, uint32(len(path)),
+		bufPtr, uint32(len(buf)),
+	)
+	return File{buf}
+}
+
+// Load the file into the given buffer.
+func loadFileInto(path string, buf []byte) File {
+	pathPtr := unsafe.Pointer(unsafe.StringData(path))
 	bufPtr := unsafe.Pointer(unsafe.SliceData(buf))
 	fileSize := loadFile(
 		pathPtr, uint32(len(path)),
