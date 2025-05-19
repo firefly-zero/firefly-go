59a2675a51f85a280833da7c17d60ee4b7c96b3d
gram <git@orsinium.dev>
2025-05-19 21:22:01 +0200

    drop RenderLine

--- firefly/callbacks.go
+++ firefly/callbacks.go
@@ -20,13 +20,6 @@ var (
 	// Callback to be called before exiting the app.
 	BeforeExit func()
 
-	// Callback to be called before rendering a horizontal line on the screen.
-	//
-	// Accepts the index of the line about to be rendered
-	// and returns the index of the line for which it should be called next time.
-	// Use it to update color palette to support more than 4 colors per frame.
-	RenderLine func(int) int
-
 	// Callback to be called when a cheat code is sent from firefly CLI.
 	//
 	// Accepts the command index and value and returns a response to show in CLI.
@@ -61,17 +54,9 @@ func beforeExit() { //nolint
 	}
 }
 
-//go:export render_line
-func renderLine(l int32) int32 { //nolint
-	if RenderLine != nil {
-		return int32(RenderLine(int(l)))
-	}
-	return 0
-}
-
 //go:export cheat
 func cheat(c, v int32) int32 { //nolint
-	if RenderLine != nil {
+	if Cheat != nil {
 		return int32(Cheat(int(c), int(v)))
 	}
 	return 0
