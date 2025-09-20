6a4c53d20e7981d7de9930683c97c0b7eebb7c00
gram <git@orsinium.dev>
2025-09-20 11:42:05 +0200

    add sixel demo

--- _examples/text/text.go
+++ _examples/text/text.go
@@ -7,10 +7,14 @@ func init() {
 	firefly.Render = render
 }
 
-var font firefly.Font
+var (
+	font  firefly.Font
+	sixel firefly.Font
+)
 
 func boot() {
 	font = firefly.LoadFile("font", nil).Font()
+	sixel = firefly.LoadFile("sixel", nil).Font()
 }
 
 func render() {
@@ -18,5 +22,10 @@ func render() {
 	firefly.DrawText(
 		"The quick brown fox\njumps over the lazy dog",
 		font, firefly.Point{X: 10, Y: 60}, firefly.ColorDarkBlue,
+	)
+	// https://www.vt100.net/dec/vt320/soft_characters
+	firefly.DrawText(
+		"???owYn||~ywo??\n?IRJaVNn^NVbJRI",
+		sixel, firefly.Point{X: 60, Y: 100}, firefly.ColorBlue,
 	)
 }
