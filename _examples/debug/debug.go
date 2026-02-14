2af6bd45eb907ef82696e97e6f6aaca455ff64d2
Kalle Fagerberg <kalle.f8+github@proton.me>
2026-02-14 11:38:44 +0100

    Add bytes overloads to LogDebug, LogError, DrawText, DrawQR

--- _examples/debug/debug.go
+++ _examples/debug/debug.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"bytes"
 	"strconv"
 
 	"github.com/firefly-zero/firefly-go/firefly"
 )
 
-var count = 0
+var (
+	count = 0
+	buf   bytes.Buffer
+)
 
 func init() {
 	firefly.Boot = boot
 	firefly.Update = update
+
+	buf.Grow(32)
 }
 
 func boot() {
@@ -21,5 +27,16 @@ func update() {
 	count++
 	if count%60 == 0 {
 		firefly.LogDebug("updates: " + strconv.Itoa(count))
+	}
+
+	if count%10 == 0 {
+		// Example of zero-allocation formatting
+		// WARN: Not recommended to write code like this.
+		// Only use this pattern in code paths where performance is critical.
+		buf.Reset()
+		buf.WriteString("... ")
+		buf.Write(strconv.AppendInt(buf.AvailableBuffer(), int64(count), 10))
+		buf.WriteString(" ...")
+		firefly.LogDebugBytes(buf.Bytes())
 	}
 }
