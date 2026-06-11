b95d9fe68649ff6ab743b1a52eb48d5ca247b40d
gram <git@orsinium.dev>
2026-06-11 14:24:18 +0200

    decouple source nodes

--- firefly/audio/nodes.go
+++ firefly/audio/nodes.go
@@ -2,14 +2,9 @@ package audio
 
 import "unsafe"
 
-type Node struct {
-	id uint32
-}
+type Node struct{ SourceNode }
 
-var Out = Node{0}
-
-// An audio node created by [Node.AddSine].
-type Sine struct{ Node }
+var Out = Node{SourceNode{0}}
 
 // An audio node created by [Node.AddMix].
 type Mix struct{ Node }
@@ -56,202 +51,143 @@ type Swap struct{ Node }
 // An audio node created by [Node.AddClip].
 type Clip struct{ Node }
 
-// An audio node created by [Node.AddSquare].
-type Square struct{ Node }
-
-// An audio node created by [Node.AddSawtooth].
-type Sawtooth struct{ Node }
-
-// An audio node created by [Node.AddTriangle].
-type Triangle struct{ Node }
-
-// An audio node created by [Node.AddNoise].
-type Noise struct{ Node }
-
-// An audio node created by [Node.AddEmpty].
-type Empty struct{ Node }
-
-// An audio node created by [Node.AddZero].
-type Zero struct{ Node }
-
-// An audio node created by [Node.AddFile].
-type File struct{ Node }
-
 // Add sine wave oscillator source (`∿`).
 func (n Node) AddSine(freq Hz, phase float32) Sine {
 	id := addSine(n.id, float32(freq), phase)
-	return Sine{Node{id}}
+	return Sine{SourceNode{id}}
 }
 
 // Add square wave oscillator source (`⎍`).
 func (n Node) AddSquare(freq Hz, phase float32) Square {
 	id := addSquare(n.id, float32(freq), phase)
-	return Square{Node{id}}
+	return Square{SourceNode{id}}
 }
 
 // Add sawtooth wave oscillator source (`╱│`).
 func (n Node) AddSawtooth(freq Hz, phase float32) Sawtooth {
 	id := addSawtooth(n.id, float32(freq), phase)
-	return Sawtooth{Node{id}}
+	return Sawtooth{SourceNode{id}}
 }
 
 // Add triangle wave oscillator source (`╱╲`).
 func (n Node) AddTriangle(freq Hz, phase float32) Triangle {
 	id := addTriangle(n.id, float32(freq), phase)
-	return Triangle{Node{id}}
+	return Triangle{SourceNode{id}}
 }
 
 // Add white noise source (amplitude on each tick is random).
 func (n Node) AddNoise(seed int) Noise {
 	id := addNoise(n.id, int32(seed))
-	return Noise{Node{id}}
+	return Noise{SourceNode{id}}
 }
 
 // Add always stopped source.
 func (n Node) AddEmpty() Empty {
 	id := addEmpty(n.id)
-	return Empty{Node{id}}
+	return Empty{SourceNode{id}}
 }
 
 // Add silent source producing zeros.
 func (n Node) AddZero() Zero {
 	id := addZero(n.id)
-	return Zero{Node{id}}
+	return Zero{SourceNode{id}}
 }
 
 // Add source playing audio from a file.
 func (n Node) AddFile(path string) File {
 	ptr := unsafe.Pointer(unsafe.StringData(path))
 	id := addFile(n.id, ptr, uint32(len(path)))
-	return File{Node{id}}
+	return File{SourceNode{id}}
 }
 
 // Add node simply mixing all inputs.
 func (n Node) AddMix() Mix {
 	id := addMix(n.id)
-	return Mix{Node{id}}
+	return Mix{Node{SourceNode{id}}}
 }
 
 // Add mixer node that stops if any of the sources stops.
 func (n Node) AddAllForOne() AllForOne {
 	id := addAllForOne(n.id)
-	return AllForOne{Node{id}}
+	return AllForOne{Node{SourceNode{id}}}
 }
 
 // Add gain control node.
 func (n Node) AddGain(lvl float32) Gain {
 	id := addGain(n.id, lvl)
-	return Gain{Node{id}}
+	return Gain{Node{SourceNode{id}}}
 }
 
 // Add a loop node that resets the input if it stops.
 func (n Node) AddLoop() Loop {
 	id := addLoop(n.id)
-	return Loop{Node{id}}
+	return Loop{Node{SourceNode{id}}}
 }
 
 // Add a node that plays the inputs one after the other, in the order as they added.
 func (n Node) AddConcat() Concat {
 	id := addConcat(n.id)
-	return Concat{Node{id}}
+	return Concat{Node{SourceNode{id}}}
 }
 
 // Add node panning the audio to the left (0.), right (1.), or something in between.
 func (n Node) AddPan(lvl float32) Pan {
 	id := addPan(n.id, lvl)
-	return Pan{Node{id}}
+	return Pan{Node{SourceNode{id}}}
 }
 
 // Add node that can be muted using modulation.
 func (n Node) AddMute() Mute {
 	id := addMute(n.id)
-	return Mute{Node{id}}
+	return Mute{Node{SourceNode{id}}}
 }
 
 // Add node that can be paused using modulation.
 func (n Node) AddPause() Pause {
 	id := addPause(n.id)
-	return Pause{Node{id}}
+	return Pause{Node{SourceNode{id}}}
 }
 
 // Add node tracking the elapsed playback time.
 func (n Node) AddTrackPosition() TrackPosition {
 	id := addTrackPosition(n.id)
-	return TrackPosition{Node{id}}
+	return TrackPosition{Node{SourceNode{id}}}
 }
 
 // Add lowpass filter node.
 func (n Node) AddLowPass(freq float32, q float32) LowPass {
 	id := addLowPass(n.id, freq, q)
-	return LowPass{Node{id}}
+	return LowPass{Node{SourceNode{id}}}
 }
 
 // Add highpass filter node.
 func (n Node) AddHighPass(freq float32, q float32) HighPass {
 	id := addHighPass(n.id, freq, q)
-	return HighPass{Node{id}}
+	return HighPass{Node{SourceNode{id}}}
 }
 
 // Add node converting stereo to mono by taking the left channel.
 func (n Node) AddTakeLeft() TakeLeft {
 	id := addTakeLeft(n.id)
-	return TakeLeft{Node{id}}
+	return TakeLeft{Node{SourceNode{id}}}
 }
 
 // Add node converting stereo to mono by taking the right channel.
 func (n Node) AddTakeRight() TakeRight {
 	id := addTakeRight(n.id)
-	return TakeRight{Node{id}}
+	return TakeRight{Node{SourceNode{id}}}
 }
 
 // Add node swapping left and right channels of the stereo input.
 func (n Node) AddSwap() Swap {
 	id := addSwap(n.id)
-	return Swap{Node{id}}
+	return Swap{Node{SourceNode{id}}}
 }
 
 // Add node clamping the input amplitude. Can be used for hard distortion.
 func (n Node) AddClip(low float32, high float32) Clip {
 	id := addClip(n.id, low, high)
-	return Clip{Node{id}}
-}
-
-// Reset the node state to how it was when it was just added.
-func (n Node) Reset() {
-	reset(n.id)
-}
-
-// Reset the node and all child nodes to the state to how it was when they were just added.
-func (n Node) ResetAll() {
-	resetAll(n.id)
-}
-
-// Remove all child nodes.
-//
-// After it is called, you should make sure to discard all references to the old
-// child nodes.
-func (n Node) Clear() {
-	clearNode(n.id)
-}
-
-// Modulate oscillation frequency.
-func (n Sine) Modulate(low, high Hz, m Modulator) {
-	m.Modulate(n.id, 0, float32(low), float32(high))
-}
-
-// Modulate oscillation frequency.
-func (n Square) Modulate(low, high Hz, m Modulator) {
-	m.Modulate(n.id, 0, float32(low), float32(high))
-}
-
-// Modulate oscillation frequency.
-func (n Sawtooth) Modulate(low, high Hz, m Modulator) {
-	m.Modulate(n.id, 0, float32(low), float32(high))
-}
-
-// Modulate oscillation frequency.
-func (n Triangle) Modulate(low, high Hz, m Modulator) {
-	m.Modulate(n.id, 0, float32(low), float32(high))
+	return Clip{Node{SourceNode{id}}}
 }
 
 // Modulate the gain level.
@@ -303,26 +239,6 @@ func (n Clip) ModulateLow(low, high float32, m Modulator) {
 // Modulate the high cut amplitude.
 func (n Clip) ModulateHigh(low, high float32, m Modulator) {
 	m.Modulate(n.id, 2, low, high)
-}
-
-// Set oscillation frequency.
-func (n Sine) Set(freq Hz) {
-	setParam(n.id, 0, float32(freq))
-}
-
-// Set oscillation frequency.
-func (n Square) Set(freq Hz) {
-	setParam(n.id, 0, float32(freq))
-}
-
-// Set oscillation frequency.
-func (n Sawtooth) Set(freq Hz) {
-	setParam(n.id, 0, float32(freq))
-}
-
-// Set oscillation frequency.
-func (n Triangle) Set(freq Hz) {
-	setParam(n.id, 0, float32(freq))
 }
 
 // Set the gain level.
---
+++ firefly/audio/source_nodes.go
@@ -0,0 +1,87 @@
+package audio
+
+type SourceNode struct {
+	id uint32
+}
+
+// An audio source node created by [Node.AddSine].
+type Sine struct{ SourceNode }
+
+// An audio source node created by [Node.AddSquare].
+type Square struct{ SourceNode }
+
+// An audio source node created by [Node.AddSawtooth].
+type Sawtooth struct{ SourceNode }
+
+// An audio source node created by [Node.AddTriangle].
+type Triangle struct{ SourceNode }
+
+// An audio source node created by [Node.AddFile].
+type File struct{ SourceNode }
+
+// An audio source node created by [Node.AddNoise].
+type Noise struct{ SourceNode }
+
+// An audio source node created by [Node.AddEmpty].
+type Empty struct{ SourceNode }
+
+// An audio source node created by [Node.AddZero].
+type Zero struct{ SourceNode }
+
+// Reset the node state to how it was when it was just added.
+func (n SourceNode) Reset() {
+	reset(n.id)
+}
+
+// Reset the node and all child nodes to the state to how it was when they were just added.
+func (n SourceNode) ResetAll() {
+	resetAll(n.id)
+}
+
+// Remove all child nodes.
+//
+// After it is called, you should make sure to discard all references to the old
+// child nodes.
+func (n SourceNode) Clear() {
+	clearNode(n.id)
+}
+
+// Modulate oscillation frequency.
+func (n Sine) Modulate(low, high Hz, m Modulator) {
+	m.Modulate(n.id, 0, float32(low), float32(high))
+}
+
+// Modulate oscillation frequency.
+func (n Square) Modulate(low, high Hz, m Modulator) {
+	m.Modulate(n.id, 0, float32(low), float32(high))
+}
+
+// Modulate oscillation frequency.
+func (n Sawtooth) Modulate(low, high Hz, m Modulator) {
+	m.Modulate(n.id, 0, float32(low), float32(high))
+}
+
+// Modulate oscillation frequency.
+func (n Triangle) Modulate(low, high Hz, m Modulator) {
+	m.Modulate(n.id, 0, float32(low), float32(high))
+}
+
+// Set oscillation frequency.
+func (n Sine) Set(freq Hz) {
+	setParam(n.id, 0, float32(freq))
+}
+
+// Set oscillation frequency.
+func (n Square) Set(freq Hz) {
+	setParam(n.id, 0, float32(freq))
+}
+
+// Set oscillation frequency.
+func (n Sawtooth) Set(freq Hz) {
+	setParam(n.id, 0, float32(freq))
+}
+
+// Set oscillation frequency.
+func (n Triangle) Set(freq Hz) {
+	setParam(n.id, 0, float32(freq))
+}
